65a6b2291f0a287c1869a325132a4a4da5d5e6e6
Kovalenko, Tim <tim.kovalenko@mda.space>
2024-01-30 14:23:11 -0500

    added a notes remap

--- lua/kap/remap.lua
+++ lua/kap/remap.lua
@@ -33,12 +33,14 @@ vim.keymap.set("n", "<leader>vpp", "<cmd>e $LOCALAPPDATA/nvim/lua/kap/packer.lua
 vim.keymap.set("n", "<leader>vpl", "<cmd>e $LOCALAPPDATA/nvim/after/plugin<CR>")
 vim.keymap.set("n", "<leader>vr", "<cmd>e $LOCALAPPDATA/nvim/lua/kap/remap.lua<CR>")
 
+
 if username == 'timam' then
     vim.keymap.set("n", "<leader>vph", "<cmd>e $LOCALAPPDATA/nvim/lua/kap/remap.lua<CR>")
     -- vim.keymap.set("n", "<leader>vmmu", "<cmd>e C:/sarnext/mmu<CR>")
 elseif username == 'ty096829' then
     vim.keymap.set("n", "<leader>vph", "<cmd>e C:/sarnext/mmu/project_help.py<CR>")
     vim.keymap.set("n", "<leader>vmmu", "<cmd>e C:/sarnext/mmu<CR>")
+    vim.keymap.set("n", "<leader>vn", "<cmd>e C:/Users/ty096829/Documents/notes.md<CR>")
 end
 
 vim.keymap.set("n", "<leader><leader>", function()

9876617d826c7418d722be07bf7f93ac723a6a21
Kovalenko, Tim <tima.mtl@gmail.com>
2024-02-12 20:35:58 -0500

    added flash

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,15 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+
+    config = function()
+        require("nvim-tree").setup {}
+        vim.keymap.set('n', '<leader>t', ":NvimTreeFindFileToggle<CR>")
+        -- vim.keymap.set('n', '<leader>t', ":NvimTreeFindFileToggle<CR> <BAR> :NvimTreeFocus<CR>")
+    end,
+
+}

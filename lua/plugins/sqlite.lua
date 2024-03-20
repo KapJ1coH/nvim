54f3b1acab5c89e4d1a1288a68c0f4b2a89801aa
Kovalenko, Tim <tima.mtl@gmail.com>
2024-03-20 10:44:21 -0400

    works on work pc

--- lua/plugins/sqlite.lua
+++ lua/plugins/sqlite.lua
@@ -2,6 +2,6 @@ return {
 	"kkharji/sqlite.lua",
 
 	config = function()
-		vim.g.sqlite_clib_path = "C:\\Users\\ty096829\\AppData\\Local\\Microsoft\\WinGet\\Links\\sqlite3.exe"
+		vim.g.sqlite_clib_path = "C:\\Users\\ty096829\\AppData\\Local\\nvim\\sqlite3.dll"
 	end,
 }

fbb0550bdf7c6eee5282113f9b66895faf75d659
Kovalenko, Tim <tima.mtl@gmail.com>
2024-04-09 12:16:05 -0400

    added some changes to telescope like frecency, fzf and filebrowser

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,72 +1,118 @@
-local builtin = require('telescope.builtin')
-local actions = require "telescope.actions"
+local builtin = require("telescope.builtin")
+local actions = require("telescope.actions")
 
 return {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim', },
-    lazy = false,
-    opts = {
-        pickers = {
-            find_files = {
-                find_command = { "fd" }
-            },
-        },
-        buffers = {
-            mappings = {
-                i = {
-                    ["<c-d>"] = actions.delete_buffer + actions.move_to_top,
-                }
-            }
-        },
-    },
-    keys = {
-        { '<leader>ff',  builtin.find_files,  {} },
-        { '<leader>lg',  builtin.live_grep,   {} },
-        { '<leader>fb',  builtin.buffers,     {} },
-        { '<leader>fh',  builtin.help_tags,   {} },
-        { '<leader>gff', builtin.git_files,   {} },
-        { '<leader>fg',  builtin.grep_string, {} },
-        -- $env:Path += ";$(Get-Location)"
-        { '<leader>fs', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") });
-        end,
-        },
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		{ "nvim-lua/plenary.nvim" },
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+		},
+		{ "nvim-telescope/telescope-file-browser.nvim" },
+		{ "nvim-telescope/telescope-frecency.nvim" },
+	},
+	lazy = false,
+	opts = {
+		pickers = {
+			find_files = {
+				find_command = { "fzf" },
+			},
+		},
+		buffers = {
+			mappings = {
+				i = {
+					["<c-d>"] = actions.delete_buffer + actions.move_to_top,
+				},
+			},
+		},
+	},
+	extensions = {
+		extensions = {
+			fzf = {
+				fuzzy = true, -- false will only do exact matching
+				override_generic_sorter = true, -- override the generic sorter
+				override_file_sorter = true, -- override the file sorter
+				case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+				-- the default case_mode is "smart_case"
+			},
+			file_browser = {
+				theme = "ivy",
+				-- disables netrw and use telescope-file-browser in its place
+				hijack_netrw = true,
+				mappings = {
+					["i"] = {
+						-- your custom insert mode mappings
+					},
+					["n"] = {
+						-- your custom normal mode mappings
+					},
+				},
+			},
+		},
+	},
 
+	keys = {
+		{ "<leader>ff", builtin.find_files, {} },
+		{ "<leader>lg", builtin.live_grep, {} },
+		{ "<leader>fb", builtin.buffers, {} },
+		{ "<leader>fh", builtin.help_tags, {} },
+		{ "<leader>gff", builtin.git_files, {} },
+		{ "<leader>fg", builtin.grep_string, {} },
+		{ "<leader>t", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", {} },
+		{ "<leader>tb", ":Telescope file_browser<CR>", {} },
+		{
+			"<leader>fr",
+			function()
+				require("telescope").extensions.frecency.frecency({})
+			end,
+			{},
+		},
+		-- $env:Path += ";$(Get-Location)"
+		{
+			"<leader>fs",
+			function()
+				builtin.grep_string({ search = vim.fn.input("Grep > ") })
+			end,
+		},
+	},
 
-    },
-    config = function()
-        -- require("tokyonight").setup({
-        --     on_highlights = function(hl, c)
-        --         local prompt = "#2d3149"
-        --         hl.TelescopeNormal = {
-        --             bg = c.bg_dark,
-        --             fg = c.fg_dark,
-        --         }
-        --         hl.TelescopeBorder = {
-        --             bg = c.bg_dark,
-        --             fg = c.bg_dark,
-        --         }
-        --         hl.TelescopePromptNormal = {
-        --             bg = prompt,
-        --         }
-        --         hl.TelescopePromptBorder = {
-        --             bg = prompt,
-        --             fg = prompt,
-        --         }
-        --         hl.TelescopePromptTitle = {
-        --             bg = prompt,
-        --             fg = prompt,
-        --         }
-        --         hl.TelescopePreviewTitle = {
-        --             bg = c.bg_dark,
-        --             fg = c.bg_dark,
-        --         }
-        --         hl.TelescopeResultsTitle = {
-        --             bg = c.bg_dark,
-        --             fg = c.bg_dark,
-        --         }
-        --     end,
-        -- })
-    end
+	config = function()
+		require("telescope").load_extension("fzf")
+		require("telescope").load_extension("file_browser")
+		require("telescope").load_extension("frecency")
+		-- require("tokyonight").setup({
+		--     on_highlights = function(hl, c)
+		--         local prompt = "#2d3149"
+		--         hl.TelescopeNormal = {
+		--             bg = c.bg_dark,
+		--             fg = c.fg_dark,
+		--         }
+		--         hl.TelescopeBorder = {
+		--             bg = c.bg_dark,
+		--             fg = c.bg_dark,
+		--         }
+		--         hl.TelescopePromptNormal = {
+		--             bg = prompt,
+		--         }
+		--         hl.TelescopePromptBorder = {
+		--             bg = prompt,
+		--             fg = prompt,
+		--         }
+		--         hl.TelescopePromptTitle = {
+		--             bg = prompt,
+		--             fg = prompt,
+		--         }
+		--         hl.TelescopePreviewTitle = {
+		--             bg = c.bg_dark,
+		--             fg = c.bg_dark,
+		--         }
+		--         hl.TelescopeResultsTitle = {
+		--             bg = c.bg_dark,
+		--             fg = c.bg_dark,
+		--         }
+		--     end,
+		-- })
+	end,
 }

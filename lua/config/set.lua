cb73486377576ddd833b981cb89939be191ddfdd
KapJ1coH <tima.mtl@gmail.com>
2024-05-15 16:25:15 -0400

    updated set to work with unix

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -16,12 +16,25 @@ vim.cmd('filetype plugin indent on')
 -- Set auto-indent options
 vim.o.autoindent = true
 vim.o.smartindent = true
-vim.o.tabstop = 4       -- Number of spaces that a <Tab> counts for
-vim.o.shiftwidth = 4    -- Number of spaces to use for each step of (auto)indent
-vim.o.expandtab = true  -- Convert tabs to spaces
+vim.o.tabstop = 4      -- Number of spaces that a <Tab> counts for
+vim.o.shiftwidth = 4   -- Number of spaces to use for each step of (auto)indent
+vim.o.expandtab = true -- Convert tabs to spaces
 vim.o.softtabstop = 4
 
 vim.opt.smartindent = true
+
+-- Windows vs Linux
+if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
+    vim.opt.shell = 'powershell'
+    vim.opt.shellcmdflag = '-Command'
+    vim.opt.shellquote = '\"'
+    vim.opt.shellxquote = ''
+    vim.opt.undodir = os.getenv("USERPROFILE") .. "/AppData/Local/nvim/undodir"
+
+elseif vim.fn.has('unix') == 1 then
+    vim.opt.shell = 'fish'
+
+end
 
 
 
@@ -40,7 +53,6 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("USERPROFILE") .. "/AppData/Local/nvim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
@@ -60,19 +72,19 @@ vim.opt.colorcolumn = ""
 vim.opt.modifiable = true
 
 vim.diagnostic.config({
-	virtual_text = false,
-	float = {
-		focusable = false,
-		style = "minimal",
-		border = "rounded",
-		source = "always",
-		header = "",
-		prefix = "",
-	},
-	signs = true,
-	underline = true,
-	update_in_insert = true,
-	severity_sort = false,
+    virtual_text = false,
+    float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+    },
+    signs = true,
+    underline = true,
+    update_in_insert = true,
+    severity_sort = false,
 })
 
 vim.keymap.set("n", "<leader>i", vim.diagnostic.open_float)

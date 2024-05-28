09c99c9b3f4d55a493d53c3e5e4fc29e06487f5b
Kovalenko, Tim <tima.mtl@gmail.com>
2024-05-28 12:00:32 -0400

    added json to conform

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,113 +1,113 @@
 -- lazy way
 return {
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v3.x",
-        -- branch = 'better-defaults',
-        lazy = true,
-        config = false,
-        init = function()
-            -- Disable automatic setup, we are doing it manually
-            vim.g.lsp_zero_extend_cmp = 0
-            vim.g.lsp_zero_extend_lspconfig = 0
-        end,
-    },
-    {
-        "williamboman/mason.nvim",
-        lazy = false,
-        config = true,
-    },
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		-- branch = 'better-defaults',
+		lazy = true,
+		config = false,
+		init = function()
+			-- Disable automatic setup, we are doing it manually
+			vim.g.lsp_zero_extend_cmp = 0
+			vim.g.lsp_zero_extend_lspconfig = 0
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = true,
+	},
 
-    -- Autocompletion
-    -- local cmp = require('cmp')
-    --
-    -- cmp.setup({
-    --   preselect = 'item',
-    --   completion = {
-    --     completeopt = 'menu,menuone,noinsert'
-    --   },
-    --   mapping = cmp.mapping.preset.insert({
-    --      ['<CR>'] = cmp.mapping.confirm({select = true}),
-    --   })
-    -- })
-    --
-    -- -- If you want insert `(` after select function or method item
-    -- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-    -- cmp.event:on(
-    --   'confirm_done',
-    --   cmp_autopairs.on_confirm_done()
-    -- )
+	-- Autocompletion
+	-- local cmp = require('cmp')
+	--
+	-- cmp.setup({
+	--   preselect = 'item',
+	--   completion = {
+	--     completeopt = 'menu,menuone,noinsert'
+	--   },
+	--   mapping = cmp.mapping.preset.insert({
+	--      ['<CR>'] = cmp.mapping.confirm({select = true}),
+	--   })
+	-- })
+	--
+	-- -- If you want insert `(` after select function or method item
+	-- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+	-- cmp.event:on(
+	--   'confirm_done',
+	--   cmp_autopairs.on_confirm_done()
+	-- )
 
-    -- LSP
-    {
-        "neovim/nvim-lspconfig",
-        cmd = { "LspInfo", "LspInstall", "LspStart" },
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "williamboman/mason-lspconfig.nvim" },
-        },
-        config = function()
-            -- This is where all the LSP shenanigans will live
-            local lsp_zero = require("lsp-zero")
-            lsp_zero.extend_lspconfig()
+	-- LSP
+	{
+		"neovim/nvim-lspconfig",
+		cmd = { "LspInfo", "LspInstall", "LspStart" },
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "williamboman/mason-lspconfig.nvim" },
+		},
+		config = function()
+			-- This is where all the LSP shenanigans will live
+			local lsp_zero = require("lsp-zero")
+			lsp_zero.extend_lspconfig()
 
-            --- if you want to know more about lsp-zero and mason.nvim
-            --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-            lsp_zero.on_attach(function(client, bufnr)
-                -- see :help lsp-zero-keybindings
-                -- to learn the available actions
-                local opts = { buffer = bufnr, remap = false }
+			--- if you want to know more about lsp-zero and mason.nvim
+			--- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+			lsp_zero.on_attach(function(client, bufnr)
+				-- see :help lsp-zero-keybindings
+				-- to learn the available actions
+				local opts = { buffer = bufnr, remap = false }
 
-                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-                vim.keymap.set("n", "<leader>lws", function()
-                    vim.lsp.buf.workspace_symbol()
-                end, opts)
-                vim.keymap.set("n", "<leader>vd", function()
-                    vim.diagnostic.open_float()
-                end, opts)
-                vim.keymap.set("n", "[d", function()
-                    vim.diagnostic.goto_next()
-                end, opts)
-                vim.keymap.set("n", "]d", function()
-                    vim.diagnostic.goto_prev()
-                end, opts)
-                vim.keymap.set("n", "<leader>lca", function()
-                    vim.lsp.buf.code_action()
-                end, opts)
-                vim.keymap.set("n", "<leader>lrr", function()
-                    vim.lsp.buf.references()
-                end, opts)
-                vim.keymap.set("n", "<leader>lrn", function()
-                    vim.lsp.buf.rename()
-                end, opts)
-                vim.keymap.set("i", "<C-h>", function()
-                    vim.lsp.buf.signature_help()
-                end, opts)
-                -- vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, opts)
-            end)
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+				vim.keymap.set("n", "<leader>lws", function()
+					vim.lsp.buf.workspace_symbol()
+				end, opts)
+				vim.keymap.set("n", "<leader>vd", function()
+					vim.diagnostic.open_float()
+				end, opts)
+				vim.keymap.set("n", "[d", function()
+					vim.diagnostic.goto_next()
+				end, opts)
+				vim.keymap.set("n", "]d", function()
+					vim.diagnostic.goto_prev()
+				end, opts)
+				vim.keymap.set("n", "<leader>lca", function()
+					vim.lsp.buf.code_action()
+				end, opts)
+				vim.keymap.set("n", "<leader>lrr", function()
+					vim.lsp.buf.references()
+				end, opts)
+				vim.keymap.set("n", "<leader>lrn", function()
+					vim.lsp.buf.rename()
+				end, opts)
+				vim.keymap.set("i", "<C-h>", function()
+					vim.lsp.buf.signature_help()
+				end, opts)
+				-- vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, opts)
+			end)
 
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pyright" },
-                handlers = {
-                    lsp_zero.default_setup,
-                    lua_ls = function()
-                        -- (Optional) Configure lua language server for neovim
-                        local lua_opts = lsp_zero.nvim_lua_ls()
-                        require("lspconfig").lua_ls.setup(lua_opts)
-                    end,
-                },
-            })
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "pyright" },
+				handlers = {
+					lsp_zero.default_setup,
+					lua_ls = function()
+						-- (Optional) Configure lua language server for neovim
+						local lua_opts = lsp_zero.nvim_lua_ls()
+						require("lspconfig").lua_ls.setup(lua_opts)
+					end,
+				},
+			})
 
-            -- Autoformat on save with Black
-            local group = vim.api.nvim_create_augroup("Black", { clear = true })
-            vim.api.nvim_create_autocmd("bufWritePost", {
-                pattern = "*.py",
-                command = "silent !black %",
-                group = group,
-            })
-        end,
-    },
+			-- -- Autoformat on save with Black
+			-- local group = vim.api.nvim_create_augroup("Black", { clear = true })
+			-- vim.api.nvim_create_autocmd("bufWritePost", {
+			--     pattern = "*.py",
+			--     command = "silent !black %",
+			--     group = group,
+			-- })
+		end,
+	},
 }

6e1d1c27c7dd90a7642ca8c07cc054b5526065f6
Kovalenko, Tim <tima.mtl@gmail.com>
2024-05-28 10:19:31 -0400

    fixed pwsh bug

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,46 +1,62 @@
 local M = {
-    "nvim-treesitter/nvim-treesitter",
-    event = "BufEnter",
-    build = ":TSUpdate",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter-context"
-    },
-    config = function()
-        local configs = require("nvim-treesitter.configs")
-        require 'nvim-treesitter.install'.prefer_git = false
-        require 'nvim-treesitter.install'.compilers = { "clang", "gcc" }
-        local context = require('treesitter-context')
+	"nvim-treesitter/nvim-treesitter",
+	event = "BufEnter",
+	build = ":TSUpdate",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-context",
+		"nushell/tree-sitter-nu",
+	},
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		require("nvim-treesitter.install").prefer_git = false
+		require("nvim-treesitter.install").compilers = { "clang", "gcc" }
+		local context = require("treesitter-context")
 
-        configs.setup({
-            ensure_installed = { "lua", "vim", "vimdoc", "query", "python", "ada", "cpp",
-                "git_config", "git_rebase", "gitignore", "html", "java", "json", "regex", "rust" },
-            sync_install = false,
-            highlight = {
-                enable = true,
-                disable = { "latex" },
-            },
-            indent = { enable = true },
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
-        })
+		configs.setup({
+			ensure_installed = {
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"python",
+				"ada",
+				"cpp",
+				"git_config",
+				"git_rebase",
+				"gitignore",
+				"html",
+				"java",
+				"json",
+				"regex",
+				"rust",
+				"nu",
+			},
+			sync_install = false,
+			highlight = {
+				enable = true,
+				disable = { "latex" },
+			},
+			indent = { enable = true },
+			-- Automatically install missing parsers when entering buffer
+			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+			auto_install = true,
+		})
 
-        context.setup({
-            enable = true,           -- Enable this plugin (Can be enabled/disabled later via commands)
-            max_lines = 0,           -- How many lines the window should span. Values <= 0 mean no limit.
-            min_window_height = 0,   -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-            line_numbers = true,
-            multiline_threshold = 5, -- Maximum number of lines to show for a single context
-            trim_scope = 'outer',    -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-            mode = 'cursor',         -- Line used to calculate context. Choices: 'cursor', 'topline'
-            -- Separator between context and content. Should be a single character string, like '-'.
-            -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-            separator = nil,
-            zindex = 20,     -- The Z-index of the context window
-            on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-        })
-    end
+		context.setup({
+			enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+			max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+			min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+			line_numbers = true,
+			multiline_threshold = 5, -- Maximum number of lines to show for a single context
+			trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+			mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+			-- Separator between context and content. Should be a single character string, like '-'.
+			-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+			separator = nil,
+			zindex = 20, -- The Z-index of the context window
+			on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+		})
+	end,
 }
-
 
 return { M }

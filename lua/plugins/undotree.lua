4e95bdde1804f4f13fdc29b188a0e25d023d7e14
KapJ1coH <tima.mtl@gmail.com>
2024-06-04 21:57:28 -0400

    refactor and fix for vimtex and undotree

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,8 +1,8 @@
 return {
-    "mbbill/undotree",
-    config = function()
-        vim.keymap.set('n', '<leader>u', ":UndotreeToggle<CR> <BAR> :UndotreeFocus<CR>")
-    end,
-    lazy = false,
+	"mbbill/undotree",
+	config = function()
+		vim.keymap.set("n", "<leader>u", ":UndotreeToggle<CR> <BAR> :UndotreeFocus<CR>")
+		vim.g.undotree_DiffCommand = "FC"
+	end,
+	lazy = false,
 }
-

196186c8da61291ee37392ae70d40622bfec6d7d
KapJ1coH <tima.mtl@gmail.com>
2024-09-10 10:18:33 -0400

    windows correction for openscad

--- lua/plugins/editor/openscad.lua
+++ lua/plugins/editor/openscad.lua
@@ -1,8 +1,14 @@
+local flag = true
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+	flag = false
+end
+
 return {
 	"salkin-mada/openscad.nvim",
 
 	dependencies = "L3MON4D3/LuaSnip",
 	lazy = false,
+	enabled = flag,
 
 	config = function()
 		require("openscad")

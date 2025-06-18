1cd19016ad7bc628bf3455d7dc5be4706ef3c55c
Kovalenko, Tim <tima.mtl@gmail.com>
2025-06-18 13:42:49 -0400

    some fixes to lsp and blink

--- lua/plugins/lsp/blink-cmp.lua
+++ lua/plugins/lsp/blink-cmp.lua
@@ -25,9 +25,17 @@ return {
         -- C-k: Toggle signature help (if signature.enabled = true)
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        keymap = { preset = 'default' },
+        keymap = {
+            preset = 'default',
+
+            ["<Enter>"] = { 'select_and_accept', "fallback" },
+        },
 
         snippets = { preset = 'luasnip' },
+
+        signature = {
+            enabled = true,
+        },
 
         appearance = {
             -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
@@ -35,8 +43,10 @@ return {
             nerd_font_variant = 'mono'
         },
 
-        -- (Default) Only show the documentation popup when manually triggered
-        completion = { documentation = { auto_show = false } },
+
+        completion = {
+            documentation = { auto_show = true, auto_show_delay_ms = 500 },
+        },
 
         -- Default list of enabled providers defined so that you can extend it
         -- elsewhere in your config, without redefining it, due to `opts_extend`
--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -19,100 +19,151 @@ return {
             -- { "hrsh7th/cmp-nvim-lsp" },
             { "mason-org/mason-lspconfig.nvim" },
         },
-        -- config = function()
-            -- -- This is where all the LSP shenanigans will live
-            -- local lsp_zero = require("lsp-zero")
-            -- lsp_zero.extend_lspconfig({
-            --     sign_text = true,
-            --     lsp_attach = lsp_attach,
-            --     capabilities = require('cmp_nvim_lsp').default_capabilities(),
-            -- })
-            -- require('lspconfig').sqlls.setup {
-            --     capabilities = capabilities,
-            --     filetypes = { 'sql' },
-            --     root_dir = function(_)
-            --         return vim.loop.cwd()
-            --     end,
-            -- }
+        config = function()
+            vim.keymap.set(
+                "n",
+                "gD",
+                vim.lsp.buf.declaration,
+                { desc = "Declaration", remap = false }
+            )
+            vim.keymap.set(
+                "n",
+                "gd",
+                vim.lsp.buf.definition,
+                { desc = "Definition", remap = false }
+            )
+            vim.keymap.set(
+                "n",
+                "gi",
+                vim.lsp.buf.implementation,
+                { desc = "Implementation", remap = false }
+            )
+            vim.keymap.set("n", "<leader>lws", function()
+                vim.lsp.buf.workspace_symbol()
+            end, { desc = "Workspace Symbol", remap = false })
 
-            -- require('lspconfig').sqls.setup {
-            --     on_init = function(client)
-            --         client.server_capabilities.documentFormattingProvider = false
-            --         client.server_capabilities.documentFormattingRangeProvider = false
-            --     end,
-            --     on_attach = function(client, bufnr)
-            --         require('sqls').on_attach(client, bufnr)
-            --     end,
-            -- }
+            vim.keymap.set("n", "<leader>vd", function()
+                vim.diagnostic.open_float()
+            end, { desc = "Open Float", remap = false })
 
+            vim.keymap.set("n", "[d", function()
+                vim.diagnostic.goto_next()
+            end, { desc = "Goto next", remap = false })
 
-            --- if you want to know more about lsp-zero and mason.nvim
-            --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-            -- lsp_zero.on_attach(function(client, bufnr)
-            --     -- see :help lsp-zero-keybindings
-            --     -- to learn the available actions
-            --     local opts = { buffer = bufnr, remap = false }
+            vim.keymap.set("n", "]d", function()
+                vim.diagnostic.goto_prev()
+            end, { desc = "Goto prev", remap = false })
 
-            --     vim.keymap.set(
-            --         "n",
-            --         "gD",
-            --         vim.lsp.buf.declaration,
-            --         { desc = "Declaration", buffer = bufnr, remap = false }
-            --     )
-            --     vim.keymap.set(
-            --         "n",
-            --         "gd",
-            --         vim.lsp.buf.definition,
-            --         { desc = "Definition", buffer = bufnr, remap = false }
-            --     )
-            --     vim.keymap.set(
-            --         "n",
-            --         "gi",
-            --         vim.lsp.buf.implementation,
-            --         { desc = "Implementation", buffer = bufnr, remap = false }
-            --     )
-            --     vim.keymap.set("n", "<leader>lws", function()
-            --         vim.lsp.buf.workspace_symbol()
-            --     end, { desc = "Workspace Symbol", buffer = bufnr, remap = false })
+            vim.keymap.set("n", "<leader>lca", function()
+                vim.lsp.buf.code_action()
+            end, { desc = "Code Action", remap = false })
 
-            --     vim.keymap.set("n", "<leader>vd", function()
-            --         vim.diagnostic.open_float()
-            --     end, { desc = "Open Float", buffer = bufnr, remap = false })
+            vim.keymap.set("n", "<leader>lrr", function()
+                vim.lsp.buf.references()
+            end, { desc = "References", remap = false })
 
-            --     vim.keymap.set("n", "[d", function()
-            --         vim.diagnostic.goto_next()
-            --     end, { desc = "Goto next", buffer = bufnr, remap = false })
+            vim.keymap.set("n", "<leader>lrn", function()
+                vim.lsp.buf.rename()
+            end, { desc = "Rename Variable", remap = false })
 
-            --     vim.keymap.set("n", "]d", function()
-            --         vim.diagnostic.goto_prev()
-            --     end, { desc = "Goto prev", buffer = bufnr, remap = false })
+            vim.keymap.set("i", "<C-h>", function()
+                vim.lsp.buf.signature_help()
+            end, { desc = "Signature Help", remap = false })
+            -- vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, opts)
+        end,
+        -- -- This is where all the LSP shenanigans will live
+        -- local lsp_zero = require("lsp-zero")
+        -- lsp_zero.extend_lspconfig({
+        --     sign_text = true,
+        --     lsp_attach = lsp_attach,
+        --     capabilities = require('cmp_nvim_lsp').default_capabilities(),
+        -- })
+        -- require('lspconfig').sqlls.setup {
+        --     capabilities = capabilities,
+        --     filetypes = { 'sql' },
+        --     root_dir = function(_)
+        --         return vim.loop.cwd()
+        --     end,
+        -- }
 
-            --     vim.keymap.set("n", "<leader>lca", function()
-            --         vim.lsp.buf.code_action()
-            --     end, { desc = "Code Action", buffer = bufnr, remap = false })
-
-            --     vim.keymap.set("n", "<leader>lrr", function()
-            --         vim.lsp.buf.references()
-            --     end, { desc = "References", buffer = bufnr, remap = false })
-
-            --     vim.keymap.set("n", "<leader>lrn", function()
-            --         vim.lsp.buf.rename()
-            --     end, { desc = "Rename Variable", buffer = bufnr, remap = false })
-
-            --     vim.keymap.set("i", "<C-h>", function()
-            --         vim.lsp.buf.signature_help()
-            --     end, { desc = "Signature Help", buffer = bufnr, remap = false })
-            --     -- vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, opts)
-            -- end)
+        -- require('lspconfig').sqls.setup {
+        --     on_init = function(client)
+        --         client.server_capabilities.documentFormattingProvider = false
+        --         client.server_capabilities.documentFormattingRangeProvider = false
+        --     end,
+        --     on_attach = function(client, bufnr)
+        --         require('sqls').on_attach(client, bufnr)
+        --     end,
+        -- }
 
 
-            -- -- Autoformat on save with Black
-            -- local group = vim.api.nvim_create_augroup("Black", { clear = true })
-            -- vim.api.nvim_create_autocmd("bufWritePost", {
-            --     pattern = "*.py",
-            --     command = "silent !black %",
-            --     group = group,
-            -- })
+        --- if you want to know more about lsp-zero and mason.nvim
+        --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+        -- lsp_zero.on_attach(function(client, bufnr)
+        --     -- see :help lsp-zero-keybindings
+        --     -- to learn the available actions
+        --     local opts = { buffer = bufnr, remap = false }
+
+        --     vim.keymap.set(
+        --         "n",
+        --         "gD",
+        --         vim.lsp.buf.declaration,
+        --         { desc = "Declaration", buffer = bufnr, remap = false }
+        --     )
+        --     vim.keymap.set(
+        --         "n",
+        --         "gd",
+        --         vim.lsp.buf.definition,
+        --         { desc = "Definition", buffer = bufnr, remap = false }
+        --     )
+        --     vim.keymap.set(
+        --         "n",
+        --         "gi",
+        --         vim.lsp.buf.implementation,
+        --         { desc = "Implementation", buffer = bufnr, remap = false }
+        --     )
+        --     vim.keymap.set("n", "<leader>lws", function()
+        --         vim.lsp.buf.workspace_symbol()
+        --     end, { desc = "Workspace Symbol", buffer = bufnr, remap = false })
+
+        --     vim.keymap.set("n", "<leader>vd", function()
+        --         vim.diagnostic.open_float()
+        --     end, { desc = "Open Float", buffer = bufnr, remap = false })
+
+        --     vim.keymap.set("n", "[d", function()
+        --         vim.diagnostic.goto_next()
+        --     end, { desc = "Goto next", buffer = bufnr, remap = false })
+
+        --     vim.keymap.set("n", "]d", function()
+        --         vim.diagnostic.goto_prev()
+        --     end, { desc = "Goto prev", buffer = bufnr, remap = false })
+
+        --     vim.keymap.set("n", "<leader>lca", function()
+        --         vim.lsp.buf.code_action()
+        --     end, { desc = "Code Action", buffer = bufnr, remap = false })
+
+        --     vim.keymap.set("n", "<leader>lrr", function()
+        --         vim.lsp.buf.references()
+        --     end, { desc = "References", buffer = bufnr, remap = false })
+
+        --     vim.keymap.set("n", "<leader>lrn", function()
+        --         vim.lsp.buf.rename()
+        --     end, { desc = "Rename Variable", buffer = bufnr, remap = false })
+
+        --     vim.keymap.set("i", "<C-h>", function()
+        --         vim.lsp.buf.signature_help()
+        --     end, { desc = "Signature Help", buffer = bufnr, remap = false })
+        --     -- vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, opts)
+        -- end)
+
+
+        -- -- Autoformat on save with Black
+        -- local group = vim.api.nvim_create_augroup("Black", { clear = true })
+        -- vim.api.nvim_create_autocmd("bufWritePost", {
+        --     pattern = "*.py",
+        --     command = "silent !black %",
+        --     group = group,
+        -- })
         -- end,
     },
 }
